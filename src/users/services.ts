7c579ba10da8f09f64b744c9d2613076ac8577ca
Luiz Antonio Ponciano Costa Bezerra <luiz.306022@alunosatc.edu.br>
2025-08-05 20:39:39 -0300

    init services users

--- src/users/services.ts
+++ src/users/services.ts
@@ -1,9 +1,3 @@
 import { supabase } from "server";
 
-export const GetUsers = async () => {
-  const { data, error } = await supabase.from("users").select("*");
-  if (error) {
-    throw new Error(error.message);
-  }
-  return data;
-};
+export const GetUsers = async () => {};
