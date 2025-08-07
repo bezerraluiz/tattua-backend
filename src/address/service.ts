65df105f6b280b02b91278a17ef1b74387bdfd76
Luiz Antonio Ponciano Costa Bezerra <luiz.306022@alunosatc.edu.br>
2025-08-07 19:31:11 -0300

    feat/init service GetAddresses

--- src/address/service.ts
+++ src/address/service.ts
@@ -1,7 +1,17 @@
+import { supabase } from "server";
+
 interface GetAddresses {
   data: [];
   status: number;
   statusText: string;
 }
 
-export const GetAddresses = async (): Promise<GetAddresses | void> => {};
+export const GetAddresses = async (): Promise<any[] | void> => {
+  const { data, error } = await supabase.from("adresses").select();
+
+  // TODO tratamento de erros
+
+  if (error) throw new Error(error.message);
+
+  return data;
+};
