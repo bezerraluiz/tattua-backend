21732bed38dc9d4e26bf96052c742f9c39b36bb2
Luiz Bezerra <luizantoniopcb@gmail.com>
2025-08-25 21:57:42 -0300

    feat/init service generate pdf quote

---
+++ src/quotes/quote.model.ts
@@ -0,0 +1,22 @@
+export interface QuotePdfParams {
+  profissional: string;
+  client_name: string;
+  date: string;
+  size: string;
+  difficulty: string;
+  body_location: string;
+  colors_quantity: string;
+  type_filling: string;
+  duration: string;
+  valores: {
+    profissional: string;
+    client_name: string;
+    date: string;
+    size: string;
+    difficulty: string;
+    body_location: string;
+    colors_quantity: string;
+    type_filling: string;
+    duration: string;
+  };
+};
